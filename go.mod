157363bf2e13275279de8b235193c52fd9622636
ashir <ashir@ispgweb.com>
2025-03-12 20:30:15 +0300

    initialization

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ashcoder666/boot.dev.go
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	// topic1 "github.com/ashcoder666/boot.dev.go/contents/structs-1"
+)
+
+func main() {
+	fmt.Println("running")
+}
