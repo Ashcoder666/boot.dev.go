664bb09aa4e85fba72410e1e0a9365085fccd308
ashcoder666 <ashirnbasheer@gmail.com>
2025-03-26 22:51:35 +0530

    done c5 tough not so tough

--- contents/slice-chapter/sli-c5/pro.go
+++ contents/slice-chapter/sli-c5/pro.go
@@ -1,5 +1,10 @@
 package slic5
 
+import (
+	"regexp"
+	"strings"
+)
+
 type sms struct {
 	id      string
 	content string
@@ -10,9 +15,34 @@ func tagMessages(messages []sms, tagger func(sms) []string) []sms {
 	for index, message := range messages {
 		messages[index].tags = tagger(message)
 	}
+
+	return messages
 }
 
 func tagger(msg sms) []string {
 	tags := []string{}
-	// ?
+	words := strings.Split(msg.content, " ")
+
+	re := regexp.MustCompile("[^a-zA-Z]+")
+	for _, word := range words {
+
+		word = re.ReplaceAllString(word, "")
+
+		if strings.ToLower(word) == "urgent" {
+			tags = append(tags, "Urgent")
+		}
+
+	}
+
+	for _, word := range words {
+
+		word = re.ReplaceAllString(word, "")
+
+		if strings.ToLower(word) == "sale" {
+			tags = append(tags, "Promo")
+		}
+	}
+
+	return tags
+
 }
