2c0e24413c1025f1bbe07fb3afb96da1dae7c80d
ashir <ashir@ispgweb.com>
2025-04-03 17:01:37 +0300

    started with concurrency

---
+++ contents/concurrency-chapter/con-2/pro.go
@@ -0,0 +1,35 @@
+package con2
+
+import (
+	"time"
+)
+
+type email struct {
+	body string
+	date time.Time
+}
+
+func checkEmailAge(emails [3]email) [3]bool {
+	isOldChan := make(chan bool)
+
+	sendIsOld(isOldChan, emails)
+
+	isOld := [3]bool{}
+
+	for i := range isOld {
+		isOld[i] = <-isOldChan
+	}
+	return isOld
+}
+
+// don't touch below this line
+
+func sendIsOld(isOldChan chan<- bool, emails [3]email) {
+	for _, e := range emails {
+		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
+			isOldChan <- true
+			continue
+		}
+		isOldChan <- false
+	}
+}
